2083cdb4b7ee80905908f96d5b1ce2c38ea5ab31
Dawid <Dawid.Dereszewski@ckw.ch>
2024-07-03 16:17:16 +0200

    Sending object to Kafka

--- src/main/java/com/dawid_ckw/kafka_poc/UserController.java
+++ src/main/java/com/dawid_ckw/kafka_poc/UserController.java
@@ -1,13 +1,19 @@
 package com.dawid_ckw.kafka_poc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private KafkaTemplate<String, MessageRequested> messageProducer;
+
     @GetMapping("/me")
     public UserResponse me(){
-        return new UserResponse(1, "Bob");
+        this.messageProducer.send("user.requested", new MessageRequested("Dawid"));
+        return new UserResponse(1, "Dawid");
     }
 }
