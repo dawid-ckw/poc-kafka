777461ac7952beaf8ef8efe4a3333c016dd76d51
Dawid <Dawid.Dereszewski@ckw.ch>
2024-07-10 22:53:38 +0200

    Download avro to user service

--- bill/src/main/java/com/dawid_ckw/kafka_poc/Config/KafkaConfig.java
+++ bill/src/main/java/com/dawid_ckw/kafka_poc/Config/KafkaConfig.java
@@ -1,24 +1,15 @@
 package com.dawid_ckw.kafka_poc.Config;
 
-import com.dawid_ckw.avro.BillRequest;
-import com.dawid_ckw.avro.BillResponse;
-import com.dawid_ckw.avro.MessageRequested;
-import com.dawid_ckw.avro.StringToUpper;
+
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,8 +33,8 @@ public class KafkaConfig {
                                                                                  ConcurrentKafkaListenerContainerFactory<String, Object> factory) {
 
         ConcurrentMessageListenerContainer<String, Object> replyContainer =
-                factory.createContainer("bill-user-bills");
-        replyContainer.getContainerProperties().setGroupId("so53151961.reply");
+                factory.createContainer("bill-user-bills-response");
+        replyContainer.getContainerProperties().setGroupId("api-bill-group-1");
         return  new ReplyingKafkaTemplate<>(pf, replyContainer);
     }
 
@@ -67,15 +58,5 @@ public class KafkaConfig {
         return configProps;
     }
 
-    @Bean
-    public Map<String, Object> consumerConfigs() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        configProps.put("schema.registry.url", "http://localhost:8085");
-        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "bill-api");
-        return configProps;
-    }
 }
 
